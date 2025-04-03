01ce914e6745369af169204e9dfe5d80a79752c9
mosajji <ali.mosaji.work@gmail.com>
2025-04-03 16:48:13 +0330

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,16 +4,16 @@ module.exports = {
   apps: [
     {
       name: "pm2-webui",
-      script: "./app.js",
+      script: "./index.js",
       env: {
         NODE_ENV: "production",
-        ENV_PATH: "./.env"
+        ENV_PATH: "./.env",
       },
       watch: false,
       ignore_watch: ["node_modules", "logs"], // جلوگیری از نظارت روی این پوشه‌ها
       autorestart: true, // ری‌استارت خودکار در صورت کرش
       instances: 1, // تعداد پردازش‌ها (۱ = سینگل پروسس، "max" = تمام هسته‌ها)
-      exec_mode: "fork" // اجرای عادی (می‌توانی به cluster تغییر دهی)
-    }
-  ]
+      exec_mode: "fork", // اجرای عادی (می‌توانی به cluster تغییر دهی)
+    },
+  ],
 };
